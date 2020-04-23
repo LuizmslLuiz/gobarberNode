f6788f8d19a5ed7d4491657872963319082acc70
Luiz <luizmsl.lm@gmail.com>
2020-04-23 16:03:31 -0300

    commit source

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-gobarberNode
+# gobarber
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+//const { Routes } = require('express');
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {  
+  return res.json({message: 'Hello Word'});
+})
+
+//module.exports = routes;
+export default routes;
